6f9770d6a6c6e0d6c0fc5fa3192cee1a9a5bd79f
Tsubasa Nakayama <app.nakayama@gmail.com>
2020-11-02 14:20:03 +0900

    add log

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,33 @@
 import express from 'express';
+import log4js from 'log4js';
+
+const logger = log4js.configure({
+    appenders: {
+        access: {
+            type: 'stdout'
+        }
+    },
+    categories: {
+        default: {
+            appenders: ['access'],
+            level: 'info'
+        }
+    }
+}).getLogger('access');
 
 const app = express();
 
+app.use(log4js.connectLogger(logger, { level: 'info' }));
+
 app.use('/', (req, res, next) => {
-    res.send("OK");
+    const log = {
+        'req.ip': req.ip,
+        'req.socket.remoteAddress': req.socket.remoteAddress,
+        'req.socket.remotePort': req.socket.remotePort,
+        'req.ips': req.ips
+    };
+    res.send(JSON.stringify(log, null, 2));
 });
 
 const port = process.env.PORT || 80;
-app.listen(port, () => { console.log(`start: http://localhost:${port}`) })
+app.listen(Number(port), '0.0.0.0', () => { console.log(`start: http://localhost:${port}`) })
