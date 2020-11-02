a157e2d113e3db6319f33179fc6238e09f41b269
Tsubasa Nakayama <app.nakayama@gmail.com>
2020-11-02 13:49:43 +0900

    add Dockerfile

--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,5 @@ app.use('/', (req, res, next) => {
     res.send("OK");
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port, () => { console.log(`start: http://localhost:${port}`) })
