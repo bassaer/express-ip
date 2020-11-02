5729d3aaf7f3232edf6887e9a86bc61ba37cf971
Tsubasa Nakayama <app.nakayama@gmail.com>
2020-11-02 11:50:19 +0900

    init

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.use('/', (req, res, next) => {
+    res.send("OK");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => { console.log(`start: http://localhost:${port}`) })
